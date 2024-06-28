1c7db011ebad6b3f4d86cdd435fbff9c9bbe5797
anuragts <anuragsharma011011@gmail.com>
2024-06-28 12:15:01 +0530

    feat: eslint prettier and husky setup

---
+++ web-app/prettier.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+	endOfLine: "lf",
+	semi: false,
+	singleQuote: false,
+	tabWidth: 2,
+	useTabs: false,
+	trailingComma: "all",
+	plugins: ["prettier-plugin-tailwindcss"],
+}
